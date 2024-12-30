f22001bc1199d7a472d15d479dd4c8eed655eb92
Aryan <aryansinghdhampur@gmail.com>
2024-12-30 20:28:57 +0530

    Adding MVC to My Appliaction

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-
 app.use('/books', router);
-
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
