845f0e6ffa358871c1d17837f670acf0679d88db
Aryan <aryansinghdhampur@gmail.com>
2024-12-31 10:52:09 +0530

    Change my db.js

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,26 @@
 import mysql from 'mysql2';
 
-const db = mysql.createConnection({
+function createDatabaseConnection(config) {
+    const db = mysql.createConnection(config);
+
+    db.connect((err) => {
+        if (err) {
+            console.error('Error connecting to MySQL Database:', err);
+        } else {
+            console.log('MySQL Database connected successfully');
+        }
+    });
+
+    return db;
+}
+
+const dbConfig = {
     host: 'localhost',
     user: 'root',
     password: 'Aryan@123',
     database: 'books',
-});
+};
 
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL Database:', err);
-    } else {
-        console.log('MySQL Database connected Successfully');
-    }
-});
+const db = createDatabaseConnection(dbConfig);
 
 export default db;
